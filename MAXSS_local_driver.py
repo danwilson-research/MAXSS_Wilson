79696df26c69c90ae27beaaacb9a7e3c2d62231b
Daniel Ford <77839711+danielford166@users.noreply.github.com>
2026-05-09 17:34:30 +0100

    Updates to allow a driver script
    - Renamed calculate hourly flux so there aren't spaces in the filename...
    - Change _main_ to a function in MAXSS_resample.py, so then the file can be imported. Added a new _main_ that shows how argparse works.
    - Updated MAXSS_run.py to a function that can be imported
    - Changed calculate_hourly_flux to a function so that it can be imported
    - Added a MAXSS_local_driver that should contain all the configuration and directory paths, and imports the three main function files so that they can be run.

---
+++ MAXSS_local_driver.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+
+Driver script for running the MAXSS analysis locally
+
+@Original author: Daniel Ford
+
+"""
+
+import MAXSS_resample as re
+import MAXSS_run as ru
+import calculate_hourly_flux_across_storm as c_flux
+
+MAXSS_working_directory = "E:/MAXSS_working_directory"
+downloadedRoot = "E:/MAXSS_working_directory/Ford_et_al_GBC_fco2/flux"
+configfiletemplate="E:/MAXSS_Wilson/MAXSS_configuration_file_template.conf"
+output_base = 'E:/MAXSS_working_directory/output/Spatially_integrated_fluxes'
+netcdf_output_root = path.join(MAXSS_working_directory, "output/Spatially_integrated_fluxes/maxss/storm-atlas/ibtracs")
+runs = ["MAXSS_RUN", "REF_RUN", "WIND_RUN", "SST_NO_GRADIENTS_RUN",
+        "SST_WITH_GRADIENTS_RUN", "SSS_RUN", "V_GAS_RUN", "PRESSURE_RUN"]
+MAXSS_regions = ["north-atlantic"]
+
+storms_to_skip = ["ALEX"]
+
+verbose = True
+
+re.MAXSS_resample_main(MAXSS_working_directory,downloadedRoot)
+
+ru.MAXSS_flux_run(MAXSS_working_directory,configfiletemplate,verbose)
+
+c_flux.calc_hourly_flux(MAXSS_working_directory,output_base,netcdf_output_root,runs,MAXSS_regions,storms_to_skip):
