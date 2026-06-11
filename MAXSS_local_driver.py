fc61abf2c3c4d0dc52a7aa9336b412058b79e513
Wilson <D.Wilson@exeter.ac.uk>
2026-06-11 11:49:32 +0100

    small changes whilst testing

--- MAXSS_local_driver.py
+++ MAXSS_local_driver.py
@@ -20,9 +20,9 @@ netcdf_output_root = path.join(MAXSS_working_directory, "output/Spatially_integr
 runs = ["MAXSS_RUN", "REF_RUN", "WIND_RUN", "SST_NO_GRADIENTS_RUN",
         "SST_WITH_GRADIENTS_RUN", "SSS_RUN", "V_GAS_RUN", "PRESSURE_RUN"]
 MAXSS_regions = ["north-atlantic"]
-specified_years = ['2010','2011']
+specified_years = ['2010']
 
-verbose = True
+verbose = False
 
 #Specify which storms you would like to run # if no storms specified, all storms run
 specified_storms = [] #["RINA","AL052010_","ALEX" "BONNIE", "MARIA", , "COLIN", "DANIELLE"]
@@ -30,9 +30,9 @@ specified_storms = [] #["RINA","AL052010_","ALEX" "BONNIE", "MARIA", , "COLIN", 
 #When set to True, only MAXSS_main run is computed and only first 5 days modelled.
 test_run = True
 
-re.MAXSS_resample_main(MAXSS_working_directory,downloadedRoot, specified_storms, MAXSS_regions, specified_years)
+#re.MAXSS_resample_main(MAXSS_working_directory,downloadedRoot, specified_storms, MAXSS_regions, specified_years)
 
-#ru.MAXSS_flux_run(MAXSS_working_directory,configfiletemplate,verbose,specified_storms,test_run)
+ru.MAXSS_flux_run(MAXSS_working_directory,configfiletemplate,verbose,specified_storms,test_run)
 
 #c_flux.calc_hourly_flux(MAXSS_working_directory,output_base,netcdf_output_root,runs,MAXSS_regions,storms_to_skip)
 
