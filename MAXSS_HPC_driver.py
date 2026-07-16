ea850381ecb20ccccab03d9f73aa0bbc522b2dfb
dw557 <dw557@athena-l01.cm.cluster>
2026-07-16 12:41:45 +0100

    ensure git last latest HPC driver

--- MAXSS_HPC_driver.py
+++ MAXSS_HPC_driver.py
@@ -22,17 +22,17 @@ runs = ["MAXSS_RUN", "REF_RUN", "WIND_RUN", "SST_NO_GRADIENTS_RUN",
 MAXSS_regions = ["north-atlantic"]
 specified_years = ['2010']
 
-verbose = True
+verbose = False
 
 #Specify which storms you would like to run # if no storms specified, all storms run
-specified_storms = [] #["RINA", "BONNIE", "MARIA", "ALEX", "COLIN", "AL052010_"]
+specified_storms = ["ALEX"] #["RINA", "BONNIE", "MARIA", "ALEX", "COLIN", "AL052010_"]
 
 #When set to True, only MAXSS_main run is computed and only first 5 days modelled.
 test_run = True
 
 #re.MAXSS_resample_main(MAXSS_working_directory,downloadedRoot, specified_storms, MAXSS_regions,specified_years)
 
-#ru.MAXSS_flux_run(MAXSS_working_directory,configfiletemplate,verbose,specified_storms,test_run)
+ru.MAXSS_flux_run(MAXSS_working_directory,configfiletemplate,verbose,specified_storms,test_run)
 
-c_flux.calc_hourly_flux(MAXSS_working_directory, output_base, netcdf_output_root,runs, MAXSS_regions, specified_storms)
+#c_flux.calc_hourly_flux(MAXSS_working_directory, output_base, netcdf_output_root,runs, MAXSS_regions, specified_storms)
 
